19b05df52c6309a6df44bcdb2d7bb5a3ff5b260e
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-01 02:10:52 +0600

    zeros and ones 2

---
+++ Zeros_and_Ones_02.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+
+    int n;
+    scanf("%d", &n);
+
+    int a[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+
+    int x;
+    scanf("%d", &x);
+
+
+    if (a[x - 1] == 0)
+    {
+        a[x - 1] = 1;
+    }
+    else if (a[x - 1] == 1)
+    {
+        a[x - 1] = 0;
+    }
+
+    
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
