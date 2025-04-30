f9ed9e47f43967d9d9c93597de3e240ea34b4d78
Masrafi404 <mdmasrafi902@gmail.com>
2025-05-01 01:03:21 +0600

    salami

---
+++ Salami.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+
+    scanf("%d %d %d", &x, &y, &z);
+    
+    printf("%d", x + y + z);
+
+    return 0;
+}
